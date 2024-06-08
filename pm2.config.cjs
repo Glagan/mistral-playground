8b53d919fe04306fe0883ed27c00f0471b1aa4b8
Glagan <Glagan@users.noreply.github.com>
2024-06-08 23:51:02 +0200

    Add pm2 configuration

---
+++ pm2.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "playground",
+    script: "build/index.js",
+    // ? DecompressionStream and CompressionStream are not supported in bun yet
+    // interpreter: "/root/.nvm/versions/node/v20.14.0/bin/bun"
+};
