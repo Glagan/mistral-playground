e03e43d4532daff326d9e6ed18ba54393bdf953c
Glagan <Glagan@users.noreply.github.com>
2025-01-19 10:44:44 +0100

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait(), sveltekit()],
+	plugins: [wasm(), sveltekit()],
+	build: {
+		target: 'esnext'
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
