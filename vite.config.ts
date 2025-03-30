307de90c7da6ec4814a01e8e9a437446823931b9
Glagan <Glagan@users.noreply.github.com>
2025-03-30 12:34:02 +0200

    Add @sveltejs/enhanced-img

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import wasm from 'vite-plugin-wasm';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [wasm(), sveltekit()],
+	plugins: [wasm(), enhancedImages(), sveltekit()],
 	build: {
 		target: 'esnext'
 	},
