9335de838ce6f52bae726eef9bd57d2e7132f9df
Glagan <Glagan@users.noreply.github.com>
2025-07-26 16:40:38 +0200

    prettier

--- src/lib/components/ui/breadcrumb/breadcrumb-item.svelte
+++ src/lib/components/ui/breadcrumb/breadcrumb-item.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-	import type { HTMLLiAttributes } from "svelte/elements";
-	import { cn, type WithElementRef } from "$lib/utils.js";
+	import type { HTMLLiAttributes } from 'svelte/elements';
+	import { cn, type WithElementRef } from '$lib/utils.js';
 
-	let {
-		ref = $bindable(null),
-		class: className,
-		children,
-		...restProps
-	}: WithElementRef<HTMLLiAttributes> = $props();
+	let { ref = $bindable(null), class: className, children, ...restProps }: WithElementRef<HTMLLiAttributes> = $props();
 </script>
 
 <li
 	bind:this={ref}
 	data-slot="breadcrumb-item"
-	class={cn("inline-flex items-center gap-1.5", className)}
+	class={cn('inline-flex items-center gap-1.5', className)}
 	{...restProps}
 >
 	{@render children?.()}
--- src/lib/components/ui/breadcrumb/breadcrumb.svelte
+++ src/lib/components/ui/breadcrumb/breadcrumb.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import type { WithElementRef } from "$lib/utils.js";
-	import type { HTMLAttributes } from "svelte/elements";
+	import type { WithElementRef } from '$lib/utils.js';
+	import type { HTMLAttributes } from 'svelte/elements';
 
 	let {
 		ref = $bindable(null),
@@ -10,12 +10,6 @@
 	}: WithElementRef<HTMLAttributes<HTMLElement>> = $props();
 </script>
 
-<nav
-	bind:this={ref}
-	data-slot="breadcrumb"
-	class={className}
-	aria-label="breadcrumb"
-	{...restProps}
->
+<nav bind:this={ref} data-slot="breadcrumb" class={className} aria-label="breadcrumb" {...restProps}>
 	{@render children?.()}
 </nav>
